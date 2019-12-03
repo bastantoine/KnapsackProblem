06b7993128c0eaee5d077acdb720c34c5c3af91a
bastantoine <bastien.antoine69@gmail.com>
2019-12-03 13:49:47 +0100

    Added main file Main.java

---
+++ Main.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ * The main class
+ * 
+ * @author bastien
+ * 
+ *
+ */
+public class Main {
+	
+	/**
+	 * 
+	 * The main method
+	 * 
+	 */
+	public static void main(String[] args) {
+
+	}
+
+}
